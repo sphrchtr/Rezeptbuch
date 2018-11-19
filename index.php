7020daaa60ad45453edd8d98d1cebd94998a9a92
BohnezMC <45168676+BohnezMC@users.noreply.github.com>
2018-11-19 20:10:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    include_once 'includes/connect.php'
+?>
+
 <!DOCTYPE html>
 
 <html lang="de">
@@ -6,12 +10,12 @@
 
 		<meta charset="utf-8"/>
 		<meta author="Gruppe_8"/>
+        <title>Gruppe_8</title>
 
 	</head>
 
 	<body>
-        
-
+           
 	</body>
 
 </html>
