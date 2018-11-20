d85d816048b2606cdf3e95655834bd66da36fbaf
BohnezMC <45168676+BohnezMC@users.noreply.github.com>
2018-11-20 17:30:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php 
     include_once 'includes/connect.php'
 ?>
@@ -6,16 +7,69 @@
 
 <html lang="de">
 
-	<head>
+<head>
 
-		<meta charset="utf-8"/>
-		<meta author="Gruppe_8"/>
-        <title>Gruppe_8</title>
+    <meta charset="utf-8" />
+    <meta author="Gruppe_8" />
+    <title>Gruppe_8</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	</head>
+</head>
 
-	<body>
-           
-	</body>
+<body>
+
+    <div class="header">
+        <!--<img src="images/header.png" width="1200" height="150"> -->
+    </div>
+    <div class="row">
+        <div class=" side_sb col-10 col-s-9">
+
+        </div>
+        <div class="searchbar col-2 col-s-3">
+            <form class="row" action="search.php" method="POST">
+                <input class="col-8 col-s-6" type="text" name="search" placeholder="Suchen">
+                <button class="col-4 col-s-6" type="submit" name="suche">
+                    <a>Suche</a>
+                </button>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col-2 col-s-3 menu">
+            <ul>
+                <li>Rezepte</li>
+                <li>Zutaten</li>
+                <li>Einkaufsliste</li>
+                <li>Kategorie</li>
+                <li>Schwierigkeit</li>
+            </ul>
+        </div>
+
+        <div class="col-7 col-s-9">
+            <h1>Rezepte.de</h1>
+            <p> At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+        </div>
+
+        <div class="col-3 col-s-12">
+            <div class="aside">
+                <h2>WAS?</h2>
+                <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <h2>WIE?</h2>
+                <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <h2>WO?</h2>
+                <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <p>Resize the browser window to see how the content respond to the resizing.</p>
+    </div>
+
+</body>
 
 </html>
+
