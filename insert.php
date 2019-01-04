44a7bb44ed994bddd4cc0c8bc0be24490e9e6b39
sphrchtr <41265553+sphrchtr@users.noreply.github.com>
2019-01-04 19:21:07 +0100

    php für Rezepteingabe
    Ja das muss auch noch bearbeitet werden, aber ich hab Urlaub, also lasst mich.

---
+++ insert.php
@@ -0,0 +1,42 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "demo");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$namerecipe = mysqli_real_escape_string($link, $_REQUEST['namerecipe']);
+$preparation = mysqli_real_escape_string($link, $_REQUEST['preparation']);
+$difficulty = mysqli_real_escape_string($link, $_REQUEST['difficulty']);
+$ingredient1 =  mysqli_real_escape_string($link, $_REQUEST['ingredient1']);
+$ingredient2 =  mysqli_real_escape_string($link, $_REQUEST['ingredient2']);
+$ingredient3 =  mysqli_real_escape_string($link, $_REQUEST['ingredient3']);
+$ingredient4 =  mysqli_real_escape_string($link, $_REQUEST['ingredient4']);
+$ingredient5 =  mysqli_real_escape_string($link, $_REQUEST['ingredient5']);
+$ingredient6 =  mysqli_real_escape_string($link, $_REQUEST['ingredient6']);
+$ingredient7 =  mysqli_real_escape_string($link, $_REQUEST['ingredient7']);
+$ingredient8 =  mysqli_real_escape_string($link, $_REQUEST['ingredient8']);
+$ingredient9 =  mysqli_real_escape_string($link, $_REQUEST['ingredient9']);
+$ingredient10 =  mysqli_real_escape_string($link, $_REQUEST['ingredient10']);
+$cathegory1 = mysqli_real_escape_string($link, $_REQUEST['cathegory1']);
+$cathegory2 = mysqli_real_escape_string($link, $_REQUEST['cathegory2']);
+$cathegory3 = mysqli_real_escape_string($link, $_REQUEST['cathegory3']);
+$time = mysqli_real_escape_string($link, $_REQUEST['time']);
+$amount = mysqli_real_escape_string($link, $_REQUEST['amount']);
+$source = mysqli_real_escape_string($link, $_REQUEST['source']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO persons (namerecipe, preparation, difficulty, ingredient1 , ingredient2 , ingredient3 , ingredient4 , ingredient5 , ingredient6 , ingredient7 , ingredient8 , ingredient9 , ingredient10 , cathegory1 , cathegory2 , cathegory3 , time , amount ,  source ) VALUES ('$namerecipe', '$preparation', '$difficulty','$ingredient1','$ingredient2','$ingredient3','$ingredient4','$ingredient5','$ingredient6','$ingredient7','$ingredient8','$ingredient9','$ingredient10','$cathegory1','$cathegory2','$cathegory3','$time','$amount', '$source')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
